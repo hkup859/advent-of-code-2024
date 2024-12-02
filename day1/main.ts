85b54a01487797d472273531bea380f774d5cac5
hkup859 <20144454+hkup859@users.noreply.github.com>
2024-12-02 10:11:32 -0500

    Format day1 with deno

--- day1/main.ts
+++ day1/main.ts
@@ -1,14 +1,13 @@
-import list1 from "./puzzle-input/list1.json" with {type: 'json'};
-import list2 from "./puzzle-input/list2.json" with {type: 'json'};
+import list1 from "./puzzle-input/list1.json" with { type: "json" };
+import list2 from "./puzzle-input/list2.json" with { type: "json" };
 
+const sortedList1 = list1.data.sort();
+const sortedList2 = list2.data.sort();
 
-const sortedList1 = list1.data.sort()
-const sortedList2 = list2.data.sort()
-
-let totalDifference = 0
+let totalDifference = 0;
 // Assumes the lists are equal length (prompt was not specific, but implied this)
-for(let i = 0; i < sortedList1.length; i++) {
-    totalDifference += Math.abs(sortedList1[i] - sortedList2[i])
+for (let i = 0; i < sortedList1.length; i++) {
+  totalDifference += Math.abs(sortedList1[i] - sortedList2[i]);
 }
 
-console.log("totalDifference: ", totalDifference)
+console.log("totalDifference: ", totalDifference);
