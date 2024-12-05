8f8b960e19ae48402f44ffe281a63b3ee4e3ffac
hkup859 <20144454+hkup859@users.noreply.github.com>
2024-12-05 10:39:36 -0500

    Finish day5 part 2, not optimized

--- day5/main.ts
+++ day5/main.ts
@@ -1,43 +1,33 @@
 const inputText = await Deno.readTextFile("./day5/puzzle-input/full_input.txt");
 
-
 const formatInput = (input: string) => {
-    const datasets = input.split('\n\n')
-    return {rules: datasets[0].split('\n'), pages: datasets[1].split('\n')}
-}
+  const datasets = input.split("\n\n");
+  return { rules: datasets[0].split("\n"), pages: datasets[1].split("\n") };
+};
 
 const solution = (input: string) => {
-    const {rules, pages } = formatInput(input)
-    // console.log("rules: ", rules)
-    // console.log("pages: ", pages)
-    
+  const { rules, pages } = formatInput(input);
 
-    let middlePageTotal = 0
-    pages.forEach((page) => {
-        // console.log("----------------")
-        // console.log("Page: ", page)
-        let safe = true
-        // Shouldn't use a foreach, should just escape
-        rules.forEach((rule) => {
-            // console.log("Rule: ", rule)
-            const rulePages = rule.split('|')
-            const page1 = rulePages[0]
-            const page2 = rulePages[1]
-            const page1Index = page.indexOf(page1)
-            const page2Index = page.indexOf(page2)
-            if (page1Index !== -1 && page2Index !== -1 && page1Index > page2Index) safe = false 
-            
-        })
-        // console.log("SAFE: ", safe)
-        if (safe) {
-            const pageArr = page.split(',')
-            middlePageTotal += Number(pageArr[Math.round((pageArr.length - 1) / 2)]);
-        }
-    })
-    return middlePageTotal
-    
-
-    
+  let middlePageTotal = 0;
+  pages.forEach((page) => {
+    let safe = true;
+    // Shouldn't use a foreach, should just escape
+    rules.forEach((rule) => {
+      const rulePages = rule.split("|");
+      const page1 = rulePages[0];
+      const page2 = rulePages[1];
+      const page1Index = page.indexOf(page1);
+      const page2Index = page.indexOf(page2);
+      if (page1Index !== -1 && page2Index !== -1 && page1Index > page2Index) {
+        safe = false;
+      }
+    });
+    if (safe) {
+      const pageArr = page.split(",");
+      middlePageTotal += Number(pageArr[Math.round((pageArr.length - 1) / 2)]);
+    }
+  });
+  return middlePageTotal;
 };
 
 console.log("Answer: ", solution(inputText));
