14a72015772887574fe9ccf26b7bc64dfcd3f6a2
hkup859 <20144454+hkup859@users.noreply.github.com>
2024-12-05 10:39:47 -0500

    Fix formatting

--- day3/main_benchmark.ts
+++ day3/main_benchmark.ts
@@ -1,14 +1,14 @@
 // Solution compatible with https://github.com/sigmaSd/Aoc2024Bench
 const solution = (input) => {
-    // Find all valid mul commands
-    const getValidMuls = input.match(/mul\([0-9]{1,3},[0-9]{1,3}\)/g);
-  
-    let total = 0;
-    getValidMuls?.forEach((mul) => {
-      const args = mul.substring(4, mul.length - 1).split(",").map(
-        (x) => Number(x),
-      );
-      total += args[0] * args[1];
-    });
-    return total;
-  };
+  // Find all valid mul commands
+  const getValidMuls = input.match(/mul\([0-9]{1,3},[0-9]{1,3}\)/g);
+
+  let total = 0;
+  getValidMuls?.forEach((mul) => {
+    const args = mul.substring(4, mul.length - 1).split(",").map(
+      (x) => Number(x),
+    );
+    total += args[0] * args[1];
+  });
+  return total;
+};
